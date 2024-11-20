ef4889bdc18c58e4c850b5f12eed1fe289874877
Josh Ehlinger <joshe@aweber.com>
2024-11-20 15:50:30 -0500

    Refactor for 2024, move to CSVs

--- README.md
+++ README.md
@@ -2,42 +2,24 @@
 
 Ho ho ho!
 
-Uses a gmail account and a json file to randomize and send secret
+Uses a gmail account and a CSV file to randomize and send secret
 santa assignments. Keep in mind that if you send emails from gmail you'll
 need to set up an App Password - https://support.google.com/accounts/answer/185833?hl=en
 
 A 5/10 on the Jank Scale.
 
-## Development
-
-Developed on Python 3.7
-
-Create a virtual environment and
-
-`pip install -r requirements.txt`
-
 ## Configuration
 
-This script uses a utility called python-dotenv to populate important
-environment variables. Create a file at the root of the directory
-called `.env` and populate it with your email credentials, and a flag
-called DRY_RUN set to `False` so the script will send emails:
+Run with `python secret_santa_driver.py APP_USERNAME APP_PASSWORD`
 
-```
-EMAIL_USERNAME='email.to.send.from@gmail.com'
-EMAIL_PASSWORD='supersecret'
-DRY_RUN=False
-```
+Use the provided `--dry-run` option to test this without actually sending a bunch of emails
 
 The emails themselves should be put into a file in the root of the
-directory called `emails.json`. This is a simple json object with a mapping
-of `name:email`, like the following:
+directory called `emails.csv`. This should have three headers, `name,email,notes`
 
 ```
-{
-    "A Name": "email@gmail.com", 
-    "Totally Real": "anotheremail@gmail.com", 
-    "Homer Simpson": "k.email.yes@gmail.com", 
-    "Another Person": "stillanemail@gmail.com"
-}
+name,email,notes
+A Name,email@gmail.com,Please dont get me any socks
+Homer Simpson,k.email.yes@gmail.com,More donuts please!
+Another Person,stillanemail@gmail.com,"Make sure, that you, escape commas"
 ```
---
+++ secret_santa.py
@@ -0,0 +1,75 @@
+import argparse
+import csv
+import logging
+import random
+import smtplib
+import sys
+from datetime import datetime
+from email import message
+
+
+def main():
+    # Parse args
+    parser = argparse.ArgumentParser()
+    parser.add_argument('user',
+                        help='App username for the gmail account')
+    parser.add_argument('password',
+                        help='App password for the gmail account')
+    parser.add_argument('--dry-run',
+                        dest='dry_run',
+                        action='store_true',
+                        help='Dont send emails, just test functionality')
+    args = parser.parse_args()
+
+    # Set up logging
+    logging.basicConfig(
+        format='[%(asctime)-15s] %(levelname)-8s [%(name)s] %(message)s',
+        level=logging.INFO,
+        stream=sys.stdout)
+
+    # Connect to gmail. Check out this support article
+    # https://support.google.com/accounts/answer/185833?hl=en when this fails
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.ehlo()
+    server.starttls()
+    server.login(args.user, args.password)
+
+    # Get the Santas and shuffle em
+    emails: list[dict] = []
+    with open('emails.csv', 'r', newline='') as file:
+        csvdict = csv.DictReader(file)
+        for row in csvdict:
+            emails.append(row)
+    random.shuffle(emails)
+
+    # Build the messages and send em
+    for idx, santa in enumerate(emails):
+        index = idx + 1
+        target = emails[0] if index == len(emails) else emails[index]
+
+        msg = message.Message()
+        msg.add_header('From', args.user)
+        msg.add_header('To', santa['email'])
+        msg.add_header('Subject', f'Secret Santa {datetime.now().year}')
+
+        msg_txt = (f"Ho ho ho, its {santa['name']}!\n\nHere's your secret "
+                   f"santa recipient: {target['name']} \n\nHappy Holidays! "
+                   "Please remember the gift should be about $25, or its "
+                   "handmade equivalent. \n\nThey provided this "
+                   f"note: {target['notes']}\n\n(This is an automated email "
+                   "pls don't respond)\n\n")
+
+        msg.set_payload(msg_txt)
+        if args.dry_run:
+            logging.info('Would send email to {}'.format(santa['email']))
+        else:
+            # Don't log the actual email here, b/c it's a secret (santa)!
+            logging.info('Sending email')
+            server.sendmail(args.user, santa['email'], msg.as_string())
+
+    server.quit()
+    logging.info('Finished sending')
+
+
+if __name__ == '__main__':
+    main()
